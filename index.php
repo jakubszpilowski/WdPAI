75c55926ee5ecaa8dfae3f651aa04d3ae87af50a
jakubszpilowski <jakub.szpilowski@student.pk.edu.pl>
2022-12-12 21:16:36 +0100

    Default routing

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo 'Hello there 👋';
+require_once 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('login', 'DefaultController');
+Routing::get('sign', 'DefaultController');
+Routing::get('home', 'DefaultController');
+Routing::get('week', 'DefaultController');
+Routing::get('settings', 'DefaultController');
+Routing::get('admin', 'DefaultController');
+Routing::get('all', 'DefaultController');
+
+Routing::run($path);
