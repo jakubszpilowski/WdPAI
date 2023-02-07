5488b8adb94c2412d742129b3bf1f30e885170b3
jakubszpilowski <jakub.szpilowski@student.pk.edu.pl>
2023-02-07 18:21:16 +0100

    Display notes on main page added

--- public/views/main_page.php
+++ public/views/main_page.php
@@ -22,14 +22,18 @@ if(!isset($_COOKIE['user'])) {
 
     <div class="container">
         <p>Latest notes</p>
-        <?php foreach ($notes as $note): ?>
+        <?php include('messages_div.php') ?>
         <section class="first">
-            <div class="note">
-                <h1><?=$note->getTitle()?></h1>
-                <h2><?=$note->getDate()?></h2>
-            </div>
+            <?php foreach ($notes as $note): ?>
+                <div class="note">
+                    <div id=<?=$note->getId(); ?> class="delete">
+                        <button id="delete-btn" class="delete-btn"></button>
+                    </div>
+                    <h1><?=$note->getTitle()?></h1>
+                    <h2><?=$note->getDate()?></h2>
+                </div>
+            <?php endforeach; ?>
         </section>
-        <?php endforeach; ?>
         <section class="second">
             <a href="all" class="action-button">
                 <strong>ALL</strong>
@@ -41,26 +45,5 @@ if(!isset($_COOKIE['user'])) {
             </button>
         </section>
     </div>
-    <div class="form-popup" id="addNote">
-        <form action="/action_page.php" class="form-container" method="POST">
-            <div class="exit">
-                <span class="add-text">Add new note</span>
-                <button class="exit-btn" onclick="closeAdd()"></button>
-            </div>
-            <div class="title-date flex">
-                <label for="title" class="title">
-                    <input type="text" placeholder="title" name="title" required>
-                </label>
-                <label for="date" class="date-input">
-                    <input type="date" name="note-date" value="2023-01-01" min="2023-01-01" max="2024-01-01" required>
-                </label>
-            </div>
-            <label for="content" class="text-area">
-                <textarea name="details" placeholder="details"></textarea>
-            </label>
-            <div class="submit-btn-div">
-                <button type="submit" class="btn"></button>
-            </div>
-        </form>
-    </div>
+    <?php include('add_note_form.php') ?>
 </body>
