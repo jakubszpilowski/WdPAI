d598e7d65fb30f8c0e0a79388c73d2773deb16f2
jakubszpilowski <jakub.szpilowski@student.pk.edu.pl>
2023-02-07 18:20:10 +0100

    Messages div moved to the separate file

---
+++ public/views/messages_div.php
@@ -0,0 +1,13 @@
+<div class="message">
+                    <?php
+                        if(isset($messages)){
+                            foreach ($messages as $message){
+                                if(isset($flag)){
+                                    echo '<span class="message-text error">' . $message . '</span>';
+                                } else{
+                                    echo '<span class="message-text">' . $message . '</span>';
+                                }
+                            }
+                        }
+                    ?>
+</div>
